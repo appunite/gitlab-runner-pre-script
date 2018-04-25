b1d55a2890879a3d341881e47d6ec2de74df6c0d
Szymon Mrozek <szymon.mrozek.sm@gmail.com>
2018-04-25 11:57:35 +0200

    Copy files from Emil's repo

---
+++ runner-vm-pre-clone.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo -e "--- Updating time&date..."
+sudo systemsetup setusingnetworktime off > /dev/null 2>&1
+sudo ntpdate -u time.apple.com > /dev/null 2>&1
+date
+
+echo -e "--- Updating CA cert..."
+sudo curl -kfsSL curl.haxx.se/ca/cacert.pem -o "$(ruby -ropenssl -e 'puts OpenSSL::X509::DEFAULT_CERT_FILE')"
+
+echo -e "--- CPU:"
+sysctl -n machdep.cpu.brand_string
+
+echo -e "--- Swift version:"
+xcrun swift -version
+
+echo -e "--- Xcodebuild version:"
+xcodebuild -version
+
+# Set bigger post buffer because of early EOF issue 
+# https://stackoverflow.com/questions/24952683/git-push-error-rpc-failed-result-56-http-code-200-fatal-the-remote-end-hun?rq=1
+
+git config --global http.postBuffer 524288000
