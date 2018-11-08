b389c8daec209368c9529dccbf66ce34ca1e672b
Piotr Bernad <piotr.bernad@appunite.com>
2018-11-08 10:08:37 +0100

    System Integrity Protection

--- runner-vm-pre-clone.sh
+++ runner-vm-pre-clone.sh
@@ -11,9 +11,16 @@ sudo curl -kfsSL curl.haxx.se/ca/cacert.pem -o "$(ruby -ropenssl -e 'puts OpenSS
 echo -e "--- System version:"
 sw_vers
 
+echo -e "--- Disable System Integrity Protection"
+csrutil disable
+
 echo -e "--- Restarting mDNSResonder"
 sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.mDNSResponder.plist
 sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.mDNSResponder.plist
+
+echo -e "--- Enable System Integrity Protection"
+csrutil enable
+
 
 echo -e "--- CPU:"
 sysctl -n machdep.cpu.brand_string
