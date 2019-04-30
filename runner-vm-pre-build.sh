a0839dce718c34f3c4bcfb1e2b0e8544e27574d0
Szymon Mrozek <szymon.mrozek.sm@gmail.com>
2019-04-30 09:27:59 +0200

    Update runner-vm-pre-build.sh

--- runner-vm-pre-build.sh
+++ runner-vm-pre-build.sh
@@ -4,4 +4,4 @@ echo "Skipping runner cache downloading"
 echo "moved to: https://git.appunite.com/appuniterd/ios/blob/master/bootstrap-project.md#makefile"
 
 echo -e "--- Checking for outdated dependencies:"
-# ruby <(curl -fsSL https://git.appunite.com/szymon.mrozek/macos-image-dependencies/raw/master/outdated_dependencies.rb) || true
+ruby <(curl -fsSL https://git.appunite.com/szymon.mrozek/macos-image-dependencies/raw/master/outdated_dependencies.rb) || true
